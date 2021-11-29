f513d880fc1ff69197d4646c49939324adb83299
The_Phoenix78 <julien.lcq@icloud.com>
2021-11-29 13:48:45 +1100

    update for android/linux users

--- BotAmino/__init__.py
+++ BotAmino/__init__.py
@@ -5,7 +5,8 @@ __title__ = 'BotAmino'
 __author__ = 'ThePhoenix78'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2021-2021 ThePhoenix78'
-__version__ = '1.16.5'
+__version__ = '1.17.0'
+
 
 from .BotAmino import *
 
