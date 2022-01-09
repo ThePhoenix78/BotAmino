8df18a4a9df2707594d39794e5e4aa35826035e9
vedansh5 <78921765+vedansh5@users.noreply.github.com>
2022-01-10 02:03:40 +0530

    Changed the version code

--- BotAmino/__init__.py
+++ BotAmino/__init__.py
@@ -4,8 +4,8 @@ from requests import get
 __title__ = 'BotAmino'
 __author__ = 'ThePhoenix78'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2021-2021 ThePhoenix78'
-__version__ = '1.20.2'
+__copyright__ = 'Copyright 2021-2022 ThePhoenix78'
+__version__ = '1.20.3'
 
 # thanks to vedansh#4039 ♥
 
