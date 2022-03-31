abfad803fd8b907bda5a3dc74713faf6ff0ddb1f
vedansh5 <78921765+vedansh5@users.noreply.github.com>
2022-03-31 16:34:39 +0530

    Deleted old files

--- BotAmino/local_amino/lib/util/__init__.py
+++
@@ -1,6 +0,0 @@
-from .exceptions import *
-from .helpers import *
-from .objects import *
-from .headers import *
-from .device import *
-from .sig_gen import *
--- BotAmino/local_amino/lib/util/device.py
+++
@@ -1,8 +0,0 @@
-class DeviceGenerator:
-    def __init__(self, deviceId=None):
-        if deviceId:
-            self.device_id = deviceId
-        else:
-            self.device_id = "32255726EEA11E60ACD268CA4DD36C8E6517144FCD24D7A53B144DE77B57980B26386188009D2BDEDE"
-
-        self.user_agent = "Dalvik/2.1.0 (Linux; U; Android 5.1.1; SM-G973N Build/beyond1qlteue-user 5; com.narvii.amino.master/3.4.33562)"
