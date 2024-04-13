f494eaf7d8532c71251047d952e18509d12824cb
Viktor <99266266+ViktorSky@users.noreply.github.com>
2024-04-13 13:47:44 -0400

    Delete BotAmino-stubs directory

--- BotAmino-stubs/bannedwords.pyi
+++
@@ -1,8 +0,0 @@
-from .parameters import Parameters
-
-__all__ = ('BannedWords',)
-
-
-class BannedWords:
-    def filtre_message(self, message: str, encoding: str) -> str: ...
-    def check_banned_words(self, data: Parameters, staff: bool = False) -> None: ...
